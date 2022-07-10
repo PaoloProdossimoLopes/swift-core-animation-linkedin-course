15ed16a638a0cc1fd80ecd9ab1f007e40404478f
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-09 21:53:51 -0300

    Feature (AnimationKeyPath): Initial cases

---
+++ swift-core-animation-demo/swift-core-animation-demo/Feature/AnimationKeyPath.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationKeyPath.swift
+//  swift-core-animation-demo
+//
+//  Created by Paolo Prodossimo Lopes on 09/07/22.
+//
+
+enum AnimationKeyPath: String {
+    
+    case opacity = "opacity"
+    case borderColor = "borderColor"
+    
+    case positionY = "position.y"
+    case positionX = "position.x"
+    
+    case scale = "transform.scale"
+}
---
+++ swift-core-animation-demo/swift-core-animation-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-core-animation-demo
+//
+//  Created by Paolo Prodossimo Lopes on 09/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
